67305a295040d21e1ffcd078b2ae86f4bfb73282
Robert Lowe <RobLowe1992@Yahoo.com>
2017-07-26 02:44:44 -0400

    Grid Fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :email, :listings
 
+    def email
+        @email = Email.new
+    end
 
+    def listings
+        @listings = JobListing.all
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @stories = Story.all
-    @email = Email.new
     @video = Video.last
-    @listings = JobListing.all
   end
 
   def destroy
